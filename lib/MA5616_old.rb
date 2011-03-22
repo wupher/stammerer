dc0c98f0d55fdae0ca338e4b46db60d576f72385
Wupher Fan <wupher.fan@gmail.com>
2011-03-22 12:09:23 +0800

    add new file, and test failure

---
+++ lib/MA5616_old.rb
@@ -0,0 +1,66 @@
+require "rubygems"
+require "tel_huawei"
+require "json"
+
+def tel_board_info(slot=0)
+  telnet_table_cmd("display board #{slot}").to_json
+end
+
+def tel_pon_sn()
+  telnet_pair_cmd('display pon sn').to_json
+end
+
+def tel_version_info(frame=0, slot=0)
+  telnet_pair_cmd("display version #{frame}/#{slot}").to_json
+end
+
+def tel_adsl_board_info(frame=0, slot=4)
+  telnet_multi_type("display board #{frame}/#{slot}").to_json
+end
+
+def tel_pstn_board_info(frame=0, slot=4)
+  adsl_board_info(frame, slot).to_json
+end
+
+def tel_adsl_port_info(frameid=0, slot=4, port=0)
+   telnet_multi_row("display interface adsl #{frameid}/#{slot}/#{port}").to_json
+end
+
+def tel_system_info()
+  #TODO: 
+  telnet_return_pair('display system sys-info').to_json
+end
+
+def tel_last_10_minutes_mem_occupancy()
+  telnet_pair_cmd('display resource occupancy mem').to_json
+end
+
+def tel_last_10_minutes_cpu_occupancy()
+  telnet_pair_cmd('display resource occupancy cpu').to_json
+end
+
+def tel_current_cpu_usage()
+  telnet_pair_cmd('display cpu 0/0').to_json
+end
+
+def tel_current_mem_usage()
+  telnet_pair_cmd('display mem 0/0').to_json
+end
+
+def tel_adsl_port_parameters(frame=0, slot=4, port=0)
+  cmd_options = {:adsl_model => true, :adsl_frame => frame, :adsl_slot => slot}
+  telnet_multi_row("display parameter #{port}",nil,cmd_options).to_json
+end
+
+def tel_adsl_port_performance(frame=0, slot=4, port=0)
+  cmd_options = {:adsl_model => true, :adsl_frame => frame, :adsl_slot  => slot}
+  telnet_pair_cmd("display statistics performance #{port} current-15minutes", nil, cmd_options).to_json
+end
+
+
+def tel_onu_port_info(frame=0, slot=0, port=1)
+  #测试结果需要过滤命令
+  cmd_options = {:gponnni_model => true, :gponnni_port => "#{frame}/#{slot}/#{port}"}
+  telnet_multi_type("display onu info",nil, cmd_options).to_json
+end
+
---
+++ lib/Telnetable.rb
@@ -0,0 +1,126 @@
+require "rubygems"
+require "net/telnet"
+require File.dirname(__FILE__)+"/retrieve_helper"
+
+module Telnetable
+  def skeleton_command(cmd, command_options)
+    raise ArgumentError, "cmd cannot be nil or empty" if cmd.nil? or cmd.empty?
+    p "Prompt:#{@prompt}"
+    telnet = Net::Telnet.new("Host" => @host, "Port" => @port, "Pormpt" => Regexp.new(@prompt), 
+    "Output_log" => File.dirname(__FILE__)+'/../logs/telnet_onu.log')
+    
+    # telnet.cmd(@user_name)
+    # telent.cmd(@password)
+    
+    telnet.login("Name" => @user_name, "Password" => @password, "LoginPrompt" => /User name:/)
+    
+    telnet.cmd('scroll 512')
+    prepare_cmd_options(telnet, command_options)
+    
+    cmd_result = []
+    yield telnet, cmd_result
+    cmd_result
+  end
+  
+  def prepare_cmd_options(telnet, command_options={})
+    enable_mode = true
+    config_mode |= command_options[:config_mode] | command_options[:adsl_mode] | command_options[:h248_mode]
+    
+    adsl_mode |= command_options[:adsl_mode]
+    adsl_board_frameid = command_options[:adsl_board_frameid]
+    adsl_board_slotid = command_options[:adsl_board_slotid]
+    raise ArgumentError, "Please specify adsl frameid & slotid to initialize adsl command mode" if adsl_board_frameid.nil? & adsl_board_slotid.nil? & adsl_mode
+    
+    h248_mode |= command_options[:h248_mode]
+    h248_mgid = command_options[:mgid]
+    raise ArgumentError, "Please specify h248 mgid" if h248_mode & h248_mgid.nil?
+    
+    gponnni_port = command_options[:gponnni_port]
+    
+    telnet.cmd('enable') if enable_mode
+    telnet.cmd('config') if config_mode
+    telnet.cmd("interface adsl #{adsl_board_frameid}/#{adsl_board_slotid}") if adsl_mode
+    telnet.cmd("interface h248 #{h248_mgid}") if h248_mode
+    telnet.cmd("interface gponnni #{gponnni_port}") if gponnni_port
+  end
+  
+  def telnet_pair_cmd(cmd, cmd_options=nil)
+    array_type_result = skeleton_command(cmd, cmd_options) do |telnet, cmd_result|
+      telnet.cmd(cmd){ |ret| tmp = retrieve_pair_info(ret); cmd_result << tmp unless tmp.empty? }
+    end
+    hash_type_result = {}
+    array_type_result.each{ |x| hash_type_result.update(x)  }
+    {"pair" => hash_type_result}
+  end
+  
+  def telnet_table_cmd(cmd, cmd_options=nil)
+    skeleton_command(cmd,cmd_options) do |telnet, cmd_result|
+      telnet.cmd(cmd){ |ret| cmd_result << ret.split(' ') if ret.split(' ').length > 2 }
+    end
+  end
+  
+  def telnet_multi_type(cmd, cmd_options=nil)
+    the_result = skeleton_command(cmd, cmd_options) do |telnet, cmd_result|
+      telnet.cmd(cmd){|ret| cmd_result << ret}
+    end
+    the_result.delete_if {|x| x.length < 10  }
+    the_result.each{ |x| x.delete!("-")  }
+    final_result = ""
+    the_result.each do |arr|
+      final_result<<arr
+    end
+    result = {}
+    pair_content, table_content = [], []
+    final_result.split("\n").each do |line|
+      next if line.empty? or line.strip.empty?
+      if line.index(':')
+        pair_content <<  retrieve_pair_info(line)
+      else
+        table_content << line.split(/\s{2}/).delete_if{|str|str.empty?}
+      end
+    end
+    result['pair'] = pair_content
+    result['table'] = table_content
+    result
+  end
+  
+  def telnet_multi_row(cmd, cmd_options=nil)
+    the_result = skeleton_command(cmd,options,cmd_options) do |telnet, cmd_result|
+       telnet.cmd(cmd){ |ret| cmd_result << ret}
+     end
+     the_result.delete_if{ |x| x.length < 10  }
+     the_result.each{ |x| x.delete("-") }
+     final_result = ""
+     the_result.each{ |arr| final_result << arr }
+     result = {}
+     pair_content, line_content = [], []
+     final_result.split("\n").each do |line|
+      next if line.empty? or line.strip.empty? or line =~ /-{20}/
+      if line.index(':')
+        pair_content << retrieve_pair_info(line) unless line.count(':') > 1
+        pair_content << retrieve_multi_pair(line) if line.count(':') > 1
+      else
+        line_content << line.strip
+      end
+     end
+     result['pair'] = pair_content
+     result['line'] = line_content
+     result
+  end
+  
+  def telnet_return_pair(cmd, cmd_options=nil)
+    the_result = skeleton_command(cmd, cmd_options) do |telnet, cmd_result|
+      telnet.cmd(cmd){ |ret| cmd_result << ret  }
+    end
+    the_result.delete_if{ |x| x.length < 2}
+    the_result.each{ |x| x.strip!}
+    the_result.delete_if{ |x| x =~ /-{10}/  }
+    result={}
+    0.upto(the_result.size-1) do |i|
+      line = the_result[i]
+      next_line = the_result[i+1]
+      result[line] = next_line if line =~ /:/
+    end
+    result
+  end
+end
