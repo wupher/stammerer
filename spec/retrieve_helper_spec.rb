0e8503a25d7465077dabbdbc35bd4d3da39063ae
Wupher Fan <wupher.fan@gmail.com>
2011-03-23 21:40:13 +0800

    add test

--- spec/retrieve_helper_spec.rb
+++ spec/retrieve_helper_spec.rb
@@ -10,6 +10,11 @@ describe "retrieve helper test" do
       accual_result.should == desire_result
     end
     
+    it "should get an empty hash if there is no seperator pair" do
+      test_str = "Profile index 1 name defval"
+      retrieve_multi_pair(test_str).should be_empty
+    end
+    
     it "should also retrieve single pair string " do
       test_str = " Profile index : 1 "
       desire = {"Profile index" => "1"}
@@ -24,6 +29,11 @@ describe "retrieve helper test" do
       desire = {"Profile index" => "1"}
       accual = retrieve_pair_info test_str
       accual.should == desire
+    end
+    
+    it "should got an empty hash if there is no seperator in str" do
+      test_str = "profile index 1"
+      retrieve_pair_info(test_str).should be_empty
     end
   end
 end
