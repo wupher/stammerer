fc5be07af8f29cd605138cd9388160c1aa2236da
Wupher Fan <wupher.fan@gmail.com>
2011-03-24 15:58:02 +0800

    add method_missing to support non json ouput

--- lib/MA5616.rb
+++ lib/MA5616.rb
@@ -14,8 +14,17 @@ class MA5616
     @password = config['password'] 
   end
   
+  def method_missing(id, *args, &block)
+    case(id.to_s)    
+    when /_to_json$/
+      send((id.to_s.split('_to_json'))[0], *args, &block).to_json
+    else
+      super
+    end
+  end
+  
   def tel_board_info(slot=0)
-    telnet_table_cmd("display board #{slot}").to_json
+    telnet_table_cmd("display board #{slot}")
   end
 
   def tel_pon_sn()
@@ -23,11 +32,11 @@ class MA5616
   end
 
   def tel_version_info(frame=0, slot=0)
-    telnet_pair_cmd("display version #{frame}/#{slot}").to_json
+    telnet_pair_cmd("display version #{frame}/#{slot}")
   end
 
   def tel_adsl_board_info(frame=0, slot=4)
-    telnet_multi_type("display board #{frame}/#{slot}").to_json
+    telnet_multi_type("display board #{frame}/#{slot}")
   end
 
   def tel_pstn_board_info(frame=0, slot=4)
@@ -35,47 +44,47 @@ class MA5616
   end
 
   def tel_adsl_port_info(frameid=0, slot=4, port=0)
-     telnet_multi_row("display interface adsl #{frameid}/#{slot}/#{port}").to_json
+     telnet_multi_row("display interface adsl #{frameid}/#{slot}/#{port}")
   end
 
   def tel_system_info()
-    telnet_return_pair('display system sys-info').to_json
+    telnet_return_pair('display system sys-info')
   end
 
   def tel_last_10_minutes_mem_occupancy()
-    telnet_pair_cmd('display resource occupancy mem').to_json
+    telnet_pair_cmd('display resource occupancy mem')
   end
 
   def tel_last_10_minutes_cpu_occupancy()
-    telnet_pair_cmd('display resource occupancy cpu').to_json
+    telnet_pair_cmd('display resource occupancy cpu')
   end
 
   def tel_current_cpu_usage()
-    telnet_pair_cmd('display cpu 0/0').to_json
+    telnet_pair_cmd('display cpu 0/0')
   end
 
   def tel_current_mem_usage()
-    telnet_pair_cmd('display mem 0/0').to_json
+    telnet_pair_cmd('display mem 0/0')
   end
 
   def tel_adsl_port_parameters(frame=0, slot=4, port=0)
     cmd_options = {:adsl_mode => true, :adsl_board_frameid => frame, :adsl_board_slotid => slot}
-    telnet_multi_row("display parameter #{port}",cmd_options).to_json
+    telnet_multi_row("display parameter #{port}",cmd_options)
   end
 
   def tel_adsl_port_performance(frame=0, slot=4, port=0)
     cmd_options = {:adsl_mode => true, :adsl_board_frameid => frame, :adsl_board_slotid  => slot}
-    telnet_pair_cmd("display statistics performance #{port} current-15minutes", cmd_options).to_json
+    telnet_pair_cmd("display statistics performance #{port} current-15minutes", cmd_options)
   end
 
 
   def tel_onu_port_info(frame=0, slot=0, port=1)
     #测试结果需要过滤命令
     cmd_options = {:gponnni_mode => true, :gponnni_port => "#{frame}/#{slot}/#{port}"}
-    telnet_multi_type("display onu info", cmd_options).to_json
+    telnet_multi_type("display onu info", cmd_options)
   end
 end
 
 # ma5616_configuration = YAML::load(File.open(File.dirname(__FILE__)+"/device_configurations/MA5616.yaml"))
 # ma5616 = MA5616.new(ma5616_configuration)
-# print ma5616.tel_adslboard_info
+# p ma5616.tel_board_info_to_json
